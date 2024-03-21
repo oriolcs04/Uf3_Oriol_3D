fb84ed8ff1f0e05140b9c616568f9243854ad821
oriolcs04 <oriolcs04@gmail.com>
2024-03-21 17:46:39 +0100

    movement fixed but not perfect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,25 +9,24 @@ public class PlayerController : MonoBehaviour
     public InputAction controls;
     public PlayerInput playerInput;
     public Vector2 playerPosition;
+
     [SerializeField]
     private Rigidbody rb;
-    //private CharacterController _cC;
     private Animator _animator;
-    private float jumpForce = 3;
-
+    //private float jumpForce = 3;
 
     [Range(0f, 13.5f)]
     public float speed;
     Vector3 moveValues;
 
-    private float _rotateSpeed = 300f;
+    private float _rotateSpeed = 300;
+    private bool increasingSpeed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
-        //_cC = GetComponent<CharacterController>();
-        _animator = GetComponent<Animator>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
 
@@ -37,28 +35,26 @@ public class PlayerController : MonoBehaviour
         playerPosition = playerInput.actions["Move"].ReadValue<Vector2>();
         _animator.SetFloat("velocity", playerPosition.magnitude * speed);
 
+        
+        moveValues = (transform.forward * playerPosition.y + transform.right * playerPosition.x) * speed;
 
-        moveValues = transform.forward * playerPosition.y + transform.right * playerPosition.x;
-        //_cC.Move(new Vector3(moveValues.x, 0, moveValues.z) * Time.deltaTime * speed + Vector3.up * Physics.gravity.y * Time.deltaTime);
-        transform.position = new Vector3(moveValues.x, 0, moveValues.z) * Time.deltaTime * speed + Vector3.up * Physics.gravity.y * Time.deltaTime;
+        Vector3 playerVelocity = new Vector3(moveValues.x, rb.velocity.y, moveValues.z);
+        rb.velocity = playerVelocity;
 
-        var keyboard = Keyboard.current;
-        if (keyboard == null)
-            return;
         RotatoinTowardsMovementDirection();
     }
 
     private void RotatoinTowardsMovementDirection()
     {
         float rotateDirection = playerInput.actions["Rotate"].ReadValue<float>();
-        transform.Rotate(Vector3.up * Time.deltaTime * _rotateSpeed * rotateDirection, Space.World);
+        transform.Rotate(Vector3.up * Time.deltaTime * _rotateSpeed * rotateDirection, Space.Self);
     }
 
     private void OnEnable()
     {
         playerInput.actions["Move"].performed += IncreaseSpeed;
-        playerInput.actions["Jump"].performed += PlayerJump;
         playerInput.actions["Move"].canceled += ResetSpeed;
+        //playerInput.actions["Jump"].performed += PlayerJump;
     }
 
     private void PlayerJump(InputAction.CallbackContext context)
@@ -78,17 +74,21 @@ public class PlayerController : MonoBehaviour
 
     private void IncreaseSpeed(InputAction.CallbackContext context)
     {
-        //_animator.SetFloat("velocity", playerPosition.magnitude * speed);
-        StartCoroutine("Run");
+        if (increasingSpeed == false)
+        {
+            StartCoroutine("Run");
+        }
     }
 
     IEnumerator Run()
     {
+        increasingSpeed = true;
         yield return new WaitForSeconds(4f);
         if (speed < 13.5f)
         {
             this.speed += 3f;
         }
+        increasingSpeed = false;
     }
 
 
