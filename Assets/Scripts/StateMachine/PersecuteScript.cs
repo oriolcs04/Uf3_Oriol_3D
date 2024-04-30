62ccf3967017cddf99f5f9fb76d7aca8867c5c2f
neyA3D <adrian.rodriguez.7e6@itb.cat>
2024-04-30 18:19:47 +0200

    Ahora anda al perseguir

--- Assets/Scripts/StateMachine/PersecuteScript.cs
+++ Assets/Scripts/StateMachine/PersecuteScript.cs
@@ -9,17 +9,20 @@ public class PersecuteScript : MonoBehaviour
     private StateMachine stateMachine;
     private Attack attack;
     private NavMeshController navMeshController;
+    private Animator animator;
 
     private void Awake()
     {
         stateMachine = GetComponent<StateMachine>();
         navMeshController = GetComponent<NavMeshController>();
         attack = GetComponent<Attack>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
         stateMachine.meshRenderIndicator.material.color = colorState;
+        animator.SetFloat("velocity", 1f);
     }
 
     private void Update()
